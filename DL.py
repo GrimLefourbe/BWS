7c044a7ffa220dbd9e44c4ee32d55d32e3bc979d
GrimLefourbe <vivienrobertbarbe+github@hotmail.fr>
2016-12-14 20:38:30 +0100

    Added Progress Feedback

--- DL.py
+++ DL.py
@@ -1,12 +1,32 @@
 import urllib.request
 import shutil
 
-
-def DownloadFile(url, filename=None):
+def DownloadFile(url, filename=None, reporthook=None):
     print("Starting download of {} to {}".format(url, filename))
     with urllib.request.urlopen(url) as response, open(filename, 'wb') as out_file:
-        shutil.copyfileobj(response, out_file)
-    print("Download finished for {}".format(filename))
+        if response.code !=200:
+            print("Error in {}".format(url))
+            print(response.code)
+            return 0
+        if reporthook==None:
+            ln = response.getheader('Content-length')
+            if ln:
+                ln=int(ln)
+                def reporthook(n):
+                    print('{}% complete'.format(100*n/ln))
+            else:
+                def reporthook(n):
+                    print('{} bytes downloaded'.format(n))
+        downloaded = 0
+        while 1:
+            chunk=response.read(16*1024)
+            if not chunk:
+                break
+            downloaded += len(chunk)
+            out_file.write(chunk)
+            reporthook(downloaded)
+
+    print("Download finished for {}, {} bytes downloaded".format(filename, downloaded))
 
     return 1
 
